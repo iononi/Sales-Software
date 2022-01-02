88a43ebd4f15cb7b7c4bc8f702f54106ac1b10b7
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-01-02 17:51:21 -0600

    Interface to extract IDs

---
+++ src/sql_connection/Extraer.java
@@ -0,0 +1,12 @@
+
+package sql_connection;
+
+/**
+ * Interfaz para extrar las primeras letras de un nombre/apellido
+ * @author Eduardo Ruiz Rios
+ */
+public interface Extraer {
+    
+    abstract String getID(String name);
+    
+}
